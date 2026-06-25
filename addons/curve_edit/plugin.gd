6dd086581d901b2788d7e5a4f1a3f27d20b019a1
pencilking2002 <pencilking2002@gmail.com>
2026-06-25 10:56:35 -0400

    Removes extraneous print statements

--- addons/curve_edit/plugin.gd
+++ addons/curve_edit/plugin.gd
@@ -6,13 +6,11 @@ var plugin : EditorInspectorPlugin
 
 
 func _enter_tree():
-	print("Curve Edit: enter tree")
 	plugin = preload("res://addons/curve_edit/path_edit.gd").new()
 	add_inspector_plugin(plugin)
 
 
 func _exit_tree():
-	print("Curve Edit: exit tree")
 	if plugin != null:
 		remove_inspector_plugin(plugin)
 
